7d07d2a42d83f851ec0cb61679addd150d466fb0
GerardoGala <ggala@hotmail.com>
2026-08-13 15:59:45 +0800

    Update index.js
    fixed the Start Button Issue

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 const params = new URLSearchParams(window.location.search);
-
 const studentEmail = params.get("studentEmail");
 const startButton = document.getElementById("startButton");
 
-if (studentEmail !== "0") {
-    document.getElementById("studentEmail").textContent = studentEmail || "StudentEmail";
-    startButton.classList.remove("d-none");
+if (studentEmail && studentEmail !== "0") {
+    if (startButton) {
+        startButton.classList.remove("d-none");
+    }
 }
